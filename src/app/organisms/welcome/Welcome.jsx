97de5d8eb2a773b0eb64468e0d7762a37444b036
stephannicolas96 <nicolas.stephan@holoffice.fr>
2023-02-28 14:32:21 +0100

    changed welcome page

--- src/app/organisms/welcome/Welcome.jsx
+++ src/app/organisms/welcome/Welcome.jsx
@@ -3,15 +3,14 @@ import './Welcome.scss';
 
 import Text from '../../atoms/text/Text';
 
-import CinnySvg from '../../../../public/res/svg/cinny.svg';
+import HolofficeSvg from '../../../../public/res/svg/holoffice.svg';
 
 function Welcome() {
   return (
     <div className="app-welcome flex--center">
       <div>
-        <img className="app-welcome__logo noselect" src={CinnySvg} alt="Cinny logo" />
-        <Text className="app-welcome__heading" variant="h1" weight="medium" primary>Welcome to Cinny</Text>
-        <Text className="app-welcome__subheading" variant="s1">Yet another matrix client</Text>
+        <img className="app-welcome__logo noselect" src={HolofficeSvg} alt="Cinny logo" />
+        <Text className="app-welcome__heading" variant="h1" weight="medium" primary>Welcome to Holoffice</Text>
       </div>
     </div>
   );

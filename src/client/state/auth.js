1347c0ee14a9359e0dd0dc6bfba76b0d54155a83
armand-colin <56818352+armand-colin@users.noreply.github.com>
2023-03-13 14:19:08 +0100

    added set store by default

--- src/client/state/auth.js
+++ src/client/state/auth.js
@@ -1,3 +1,4 @@
+import { updateLocalStore } from '../action/auth';
 import cons from './cons';
 
 const urlParameters = new URLSearchParams(window.location.search);
@@ -10,13 +11,21 @@ function getUrl(key) {
   return urlParameters.get(key)
 }
 
-const isAuthenticated = () => getSecret(cons.secretKey.ACCESS_TOKEN) !== null || !!getUrl("accessToken");
+const isAuthenticated = () => getSecret(cons.secretKey.ACCESS_TOKEN);
+
+const accessToken = getUrl('accessToken')
+const deviceId = getUrl('deviceId')
+const userId = getUrl('userId')
+const baseUrl = getUrl('baseUrl')
+
+if (accessToken && deviceId && userId && baseUrl)
+  updateLocalStore(accessToken, deviceId, userId, baseUrl)
 
 const secret = {
-  accessToken: getSecret(cons.secretKey.ACCESS_TOKEN) ?? getUrl("accessToken"),
-  deviceId: getSecret(cons.secretKey.DEVICE_ID) ?? getUrl("deviceId"),
-  userId: getSecret(cons.secretKey.USER_ID) ?? getUrl("userId"),
-  baseUrl: getSecret(cons.secretKey.BASE_URL) ?? getUrl("baseUrl"),
+  accessToken: getSecret(cons.secretKey.ACCESS_TOKEN),
+  deviceId: getSecret(cons.secretKey.DEVICE_ID),
+  userId: getSecret(cons.secretKey.USER_ID),
+  baseUrl: getSecret(cons.secretKey.BASE_URL),
 };
 
 export {
